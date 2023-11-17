5c6241eadcc58affdfa14aaa8d2f7ce4100c349e
thomas <thomas@tpoe.dev>
2023-11-17 09:34:23 +0100

    chore(animation-manager): remove logging

--- applications/services/desktop/animations/animation_manager.c
+++ applications/services/desktop/animations/animation_manager.c
@@ -390,14 +390,9 @@ static StorageAnimation*
     uint32_t whole_weight = 0;
 
     bool fallback = XTREME_SETTINGS()->fallback_anim;
-    FURI_LOG_E(TAG, "test avoid animation: ");
-    FURI_LOG_E(TAG, "%d", dolphin_internal_size);
-    FURI_LOG_E(TAG, "%d", StorageAnimationList_size(animation_list));
-
     bool unlock = XTREME_SETTINGS()->unlock_anims;
 
     uint32_t valid_animations = 0;
-
     for
         M_EACH(item, animation_list, StorageAnimationList_t) {
             const StorageAnimationManifestInfo* manifest_info = animation_storage_get_meta(*item);
@@ -407,14 +402,13 @@ static StorageAnimation*
             };
         }
 
-    FURI_LOG_E(TAG, "valid animations: %ld", valid_animations);
     if(valid_animations <= 2 && !fallback) {
         // One ext anim and fallback disabled, dont skip current anim (current = only ext one)
         avoid_animation = NULL;
-        FURI_LOG_E(TAG, "clear avoid animation");
     }
 
     if(StorageAnimationList_size(animation_list) == dolphin_internal_size + 1 && !fallback) {
+        // One ext anim and fallback disabled, dont skip current anim (current = only ext one)
         avoid_animation = NULL;
     }
 
@@ -425,9 +419,6 @@ static StorageAnimation*
             animation_storage_get_meta(storage_animation);
         bool valid = animation_manager_is_valid_idle_animation(manifest_info, &stats, unlock);
 
-        if(avoid_animation != NULL) {
-            FURI_LOG_E(TAG, avoid_animation);
-        }
         if(avoid_animation != NULL && strcmp(manifest_info->name, avoid_animation) == 0) {
             // Avoid repeating same animation twice
             valid = false;
